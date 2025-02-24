9c324719f946f00c04f1b013f0475d101d1f174f
Jack Gallant <concise_sparsity287626@outlook.com>
2025-02-24 15:12:05 +0000

    added basic login page

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,15 @@
-var places = [
-
-];
-
-const list = document.getElementById("places-list");
-
+function toggleForm() {
+    const formTitle = document.getElementById("form-title");
+    const nameField = document.getElementById("name-field");
+    const toggleText = document.getElementById("toggle-form");
+    
+    if (nameField.style.display === "none") {
+        nameField.style.display = "block";
+        formTitle.textContent = "Sign Up";
+        toggleText.innerHTML = "Already have an account? <a href='#' onclick='toggleForm()'>Login</a>";
+    } else {
+        nameField.style.display = "none";
+        formTitle.textContent = "Login";
+        toggleText.innerHTML = "Don't have an account? <a href='#' onclick='toggleForm()'>Sign Up</a>";
+    }
+}

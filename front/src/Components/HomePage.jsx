f177e95e1222f10116b253c58b9b48552392fc00
Rosie Allison-Corio <rosievespa@gmail.com>
2025-02-25 13:10:40 +0000

    added home page style

--- front/src/Components/HomePage.jsx
+++ front/src/Components/HomePage.jsx
@@ -1,22 +1,24 @@
 export default function HomePage() {
     return (
         <>
-            <section className="homePointsContainer">
-                <p>Your points..</p>
-                {/* points display goes here */}
-            </section>
-            <section>
-                <p>Lets get cracking!</p>
-                <div>
-                    <p>Task 1</p>
-                </div>
-                <div>
-                    <p>Task 2</p>
-                </div>
-                <div>
-                    <p>Task 3</p>
-                </div>
-            </section>
+            <div className="homePage">
+                <section className="homePointsContainer">
+                    <p>Your points..</p>
+                    {/* points display goes here */}
+                </section>
+                    <p>Lets get cracking!</p>
+                <section className="homeTaskContainer">
+                    <div className="homeTask">
+                        <p>Task 1</p>
+                    </div>
+                    <div className="homeTask"> 
+                        <p>Task 2</p>
+                    </div>
+                    <div className="homeTask">
+                        <p>Task 3</p>
+                    </div>
+                </section>
+            </div>
         </>
     )
 }

e9654538a53703b25ac4910b6410b29769b56ba7
Jack Gallant <concise_sparsity287626@outlook.com>
2025-02-25 14:36:50 +0000

    login fix

--- front/src/Components/LoginPage.jsx
+++ front/src/Components/LoginPage.jsx
@@ -43,34 +43,6 @@ export default function LoginPage() {
   }
 
   return (
-<<<<<<< Updated upstream
-    <>
-      <div class="container">
-        <h1 id="form-title">Login</h1>
-        <form id="auth-form">
-          <div id="name-field" class="input-group" style="display: none">
-            <label for="full-name">Full Name</label>
-            <input type="text" id="full-name" name="full-name" />
-          </div>
-          <div class="input-group">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required />
-          </div>
-          <div class="input-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-        <p id="toggle-form">
-          Don't have an account?{" "}
-          <a href="#" onclick="toggleForm()">
-            Sign Up
-          </a>
-        </p>
-      </div>
-    </>
-=======
     <div className="container">
       <h1>{isSignUp ? "Sign Up" : "Login"}</h1>
       <form id="auth-form" onSubmit={handleSignIn}>
@@ -111,6 +83,5 @@ export default function LoginPage() {
         </a>
       </p>
     </div>
->>>>>>> Stashed changes
   );
 }
